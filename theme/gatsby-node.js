ec72730fd6e5c6fde7e7fa8ddc5130f2ef0c14ed
ethriel3695 <ethriel3695@gmail.com>
2019-07-09 05:43:25 -0600

    Added create pages code

--- theme/gatsby-node.js
+++ theme/gatsby-node.js
@@ -1,5 +1,44 @@
+// https://egghead.io/lessons/gatsby-set-up-to-create-data-driven-pages-in-gatsby
+
+const fs = require("fs");
+
+// Verify the data directory exists
+exports.onPreBootstrap = ({ reporter }) => {
+  const contentPath = "data";
+
+  if (!fs.existsSync(contentPath)) {
+    reporter.info(`creating the ${contentPath} directory`);
+    fs.mkdirSync(contentPath);
+  }
+};
+
+// Define the event type
+exports.sourceNodes = ({ actions }) => {
+  actions.createTypes(`
+    type Navigation implements Node @dontInfer {
+      id: ID!
+      route: String!
+      label: String!
+    }`);
+  // actions.createTypes(`
+  // type Affirmation implements Node @dontInfer {
+  //   id: ID!
+  //   route: String!
+  //   label: String!
+  //   dueDate: Date! @dateformat @proxy(from: "due_date")
+  // }`);
+};
+
+// Define resolvers for any custom fields
+exports.createResolvers = ({ createResolvers }) => {
+  const basePath = "/";
+
+  const slugify = str => {};
+};
+
+// Query for events and create pages
 exports.createPages = ({ actions, reporter }) => {
-  reporter.warn("make sure to load data from somewhere!")
+  reporter.warn("make sure to load data from somewhere!");
 
   // TODO replace this with data from somewhere
   actions.createPage({
@@ -17,5 +56,5 @@ exports.createPages = ({ actions, reporter }) => {
         </p>
       `,
     },
-  })
-}
+  });
+};
---
+++ theme/src/templates/callback.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import Layout from "../components/layout";
+import LoadingProgress from "../components/Progress/LoadingProgress";
+import { withStyles } from "@material-ui/core/styles";
+import { handleAuthentication } from "../components/Auth/Auth";
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  list: {
+    width: 250,
+  },
+};
+
+class Callback extends Component {
+  render() {
+    handleAuthentication();
+    return (
+      <Layout>
+        <div>
+          <LoadingProgress />
+        </div>
+      </Layout>
+    );
+  }
+}
+
+export default withStyles(styles)(Callback);
